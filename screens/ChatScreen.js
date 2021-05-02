5bab3f4a1c18a52163b2028784bcdf0507831aaf
maneeshapriwoo <maneesha@priwoo.com>
2021-05-02 23:56:37 +0530

    add topp headers adn btns

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Avatar } from 'react-native-elements'
+import { TouchableOpacity } from 'react-native'
+import { Entypo, AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation, route }) => {
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Chat",
+            headerBackTitleVisible: false,
+            headerTitlealign: "left",
+            headerTitle: () => (
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar
+
+                        rounded source={{
+                            uri: "https://www.facebook.com/photo?fbid=2995756637325500&set=a.1391312221103291"
+                        }} />
+                    <Text
+                        style={{ color: "white", marginLeft: 10, fontWeight: "600" }}
+                    >{route.params.chatName}</Text>
+                </View>
+            ),
+            headerLeft: () => (
+                <TouchableOpacity
+                    style={{ marginLeft: 10 }}
+                    onPress={navigation.goBack}
+                >
+                    <AntDesign name="arrowleft" size={24} color="white" />
+                </TouchableOpacity>
+            ), headerRight: () => (
+                <View style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: 70,
+                    marginRight: 10
+                }}>
+                    <TouchableOpacity>
+                        <FontAwesome name="video-camera" size={24} color="white" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="call" size={24} color="white" />
+                    </TouchableOpacity>
+                </View>
+            )
+
+        });
+    }, [navigation])
+
+
     return (
         <View>
-            <Text>Chat screen</Text>
+            <Text>{route.params.chatName}</Text>
         </View>
     )
 }
